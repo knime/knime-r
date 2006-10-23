30392204349d0bfe83fe7c18429c837985349892
Peter Ohl <peter.ohl@knime.com>
2006-10-23 15:50:37 +0000

    moved ColumnFilterPanel from core.data.util to core.node.util

--- org.knime.ext.r/src/org/knime/ext/r/node/RPlotterNodeDialog.java
+++ org.knime.ext.r/src/org/knime/ext/r/node/RPlotterNodeDialog.java
@@ -30,11 +30,11 @@ import java.util.Set;
 
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.DoubleValue;
-import org.knime.core.data.util.ColumnFilterPanel;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.NotConfigurableException;
+import org.knime.core.node.util.ColumnFilterPanel;
 
 
 /**
