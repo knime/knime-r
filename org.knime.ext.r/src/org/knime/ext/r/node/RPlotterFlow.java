87a5854d90978ac82f66fb4746fcbb9eec4507be
Thorsten Meinl <thorsten.meinl@knime.com>
2008-02-14 15:20:55 +0000

    Version bump and copyright year update

--- org.knime.ext.r/src/org/knime/ext/r/node/RPlotterFlow.java
+++ org.knime.ext.r/src/org/knime/ext/r/node/RPlotterFlow.java
@@ -1,6 +1,6 @@
 /*
  * -------------------------------------------------------------------
- * Copyright, 2003 - 2007
+ * Copyright, 2003 - 2008
  * University of Konstanz, Germany.
  * Chair for Bioinformatics and Information Mining
  * Prof. Dr. Michael R. Berthold
