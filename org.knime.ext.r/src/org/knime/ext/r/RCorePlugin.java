941054fc79de72c80c30ccc7d8a5e201171191fa
Bernd Wiswedel <bernd.wiswedel@knime.com>
2013-07-12 15:26:14 +0000

    fix import

--- org.knime.ext.r/src/org/knime/ext/r/RCorePlugin.java
+++ org.knime.ext.r/src/org/knime/ext/r/RCorePlugin.java
@@ -28,7 +28,6 @@ import java.io.File;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.knime.core.node.NodeFactory;
-import org.knime.ext.r.bin.PackagedPathUtil;
 import org.knime.ext.r.node.RScriptingNodeFactory;
 import org.osgi.framework.BundleContext;
 
@@ -95,7 +94,7 @@ public class RCorePlugin extends AbstractUIPlugin {
      */
     public static File getRExecutable() {
         try {
-            File packagedExecutable = PackagedPathUtil.getPackagedExecutable();
+            File packagedExecutable = SystemPathUtil.getSystemExecutable();
             if (packagedExecutable != null) {
                 return packagedExecutable;
             }
