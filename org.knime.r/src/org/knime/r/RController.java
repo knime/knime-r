24eaf9060dae0edbd6f3edcccf4a8f1fe9afe701
Bernd Wiswedel <bernd.wiswedel@knime.com>
2013-07-22 11:00:04 +0000

    fix spelling in error message

--- org.knime.r/src/org/knime/r/RController.java
+++ org.knime.r/src/org/knime/r/RController.java
@@ -349,7 +349,7 @@ public class RController {
 
 	private boolean checkRHome(final String rHomePath) {
 		File rHome = new File(rHomePath);
-		String msgSuffix = "R_HOME is meant to be the path to the folder beeing the root of Rs installation tree. \n"
+		String msgSuffix = "R_HOME is meant to be the path to the folder which is the root of Rs installation tree. \n"
 				+ "It contains a bin folder which itself contains the R executable. \nPlease change the R settings in the preferences.";
 		if (!rHome.exists()) {
 			m_errors.add("R_HOME does not exists. \n" + msgSuffix);
