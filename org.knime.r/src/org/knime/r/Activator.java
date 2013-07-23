b6abc8c4671618ee988f9ff41be81b76303bd573
Thomas Gabriel <thomas.gabriel@knime.com>
2013-07-23 09:41:17 +0000

    fixed path problems, implement PreferenceListener (new R labs)

--- org.knime.r/src/org/knime/r/Activator.java
+++ org.knime.r/src/org/knime/r/Activator.java
@@ -2,14 +2,17 @@ package org.knime.r;
 
 import java.io.File;
 
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.knime.ext.r.bin.RPathUtil;
+import org.knime.r.preferences.RPreferenceInitializer;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends AbstractUIPlugin implements IPropertyChangeListener {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.knime.r"; //$NON-NLS-1$
@@ -31,7 +34,8 @@ public class Activator extends AbstractUIPlugin {
     public void start(final BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-
+		getPreferenceStore().addPropertyChangeListener(this);
+		R_HOME = new File(getPreferenceStore().getString(RPreferenceInitializer.PREF_R_HOME));
 	}
 
 	/*
@@ -40,6 +44,7 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	@Override
     public void stop(final BundleContext context) throws Exception {
+		getPreferenceStore().removePropertyChangeListener(this);
 		plugin = null;
 		super.stop(context);
 	}
@@ -54,13 +59,29 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/**
-     * @return R executable
-     */
-    public static File getRHOME() {
+	 * @return default R_HOME either package bundle or system default
+	 */
+    public static File getDefaultRHOME() {
         File packagedExecutable = RPathUtil.getPackagedRHome();
         if (packagedExecutable != null) {
             return packagedExecutable;
         }
         return RPathUtil.getSystemRHome();
+    }
+    
+    private static File R_HOME = getDefaultRHOME();
+    
+    /**
+     * @return R executable
+     */
+    public static File getRHOME() {
+    	return R_HOME;
+    }
+    
+    @Override
+    public void propertyChange(final PropertyChangeEvent event) {
+    	if (RPreferenceInitializer.PREF_R_HOME.equals(event.getProperty())) {
+    		R_HOME = new File(event.getNewValue().toString());
+    	}
     }
 }
