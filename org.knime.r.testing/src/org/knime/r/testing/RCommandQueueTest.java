238d0ec396460cb41345b2b0c2a3cded35edfe09
Thorsten Meinl <thorsten.meinl@knime.com>
2018-02-01 14:08:35 +0100

    AP-8784: Added missing copyright headers
    AP-8784 (R Integration: Development Setup uses R 2.15 binaries instead of 3.4.2 binaries on Windows)
    https://knime-com.atlassian.net/browse/AP-8784

--- org.knime.r.testing/src/org/knime/r/testing/RCommandQueueTest.java
+++ org.knime.r.testing/src/org/knime/r/testing/RCommandQueueTest.java
@@ -1,3 +1,48 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ */
 package org.knime.r.testing;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -26,7 +71,7 @@ import org.rosuda.REngine.Rserve.protocol.RTalk;
 
 /**
  * Test for {@link RCommandQueue}.
- * 
+ *
  * @author Jonathan Hale
  */
 public class RCommandQueueTest {
@@ -80,7 +125,7 @@ public class RCommandQueueTest {
 	 * <li>Execution of commands in the command queue</li>
 	 * <li>Handling of foul commands and other failure states.</li>
 	 * </ul>
-	 * 
+	 *
 	 * @throws InterruptedException
 	 * @throws ExecutionException
 	 * @throws TimeoutException
@@ -170,7 +215,7 @@ public class RCommandQueueTest {
 	/**
 	 * Test for a bug which caused multiple commands executed sequentially to
 	 * result in a infinitely blocked SocketInputStream inside {@link RTalk}.
-	 * 
+	 *
 	 * @throws InterruptedException
 	 * @throws ExecutionException
 	 * @throws TimeoutException
@@ -213,7 +258,7 @@ public class RCommandQueueTest {
 	/**
 	 * Test for a bug which caused the R Console Execution thread to block until
 	 * the current R script has finished executing.
-	 * 
+	 *
 	 * @throws InterruptedException
 	 * @throws ExecutionException
 	 * @throws TimeoutException
@@ -232,7 +277,7 @@ public class RCommandQueueTest {
 
 	/**
 	 * Test whether the RCommandQueue handles invalid R code correctly.
-	 * 
+	 *
 	 * @throws InterruptedException
 	 * @throws ExecutionException
 	 * @throws TimeoutException
--- org.knime.r.testing/src/org/knime/r/testing/RConnectionFactoryTest.java
+++ org.knime.r.testing/src/org/knime/r/testing/RConnectionFactoryTest.java
@@ -1,3 +1,48 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ */
 package org.knime.r.testing;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -28,7 +73,7 @@ public class RConnectionFactoryTest {
 	/**
 	 * Test result of {@link RConnectionFactory#createConnection()}, if the
 	 * underlying Rserve server was started correctly.
-	 * 
+	 *
 	 * @throws RserveException
 	 * @throws InterruptedException
 	 * @throws IOException
@@ -82,14 +127,14 @@ public class RConnectionFactoryTest {
 	/**
 	 * Test whether the Rserve process created for a connection can be correctly
 	 * terminated.
-	 * 
+	 *
 	 * <p>
 	 * <b>IMPORTANT:</b> When this test fails, one or more Rserve processes have
 	 * been leaked in this <b>and/or</b> in one of the other tests. The Rserve
 	 * processes have to be terminated manually and the org.knime.r tests should
 	 * be deactivated until this is fixed.
 	 * </p>
-	 * 
+	 *
 	 * @throws RserveException
 	 * @throws InterruptedException
 	 * @throws IOException
@@ -110,7 +155,7 @@ public class RConnectionFactoryTest {
 	/**
 	 * Test whether the Rserve process created for a connection creates a new
 	 * workspace, therefore no variables are leaked.
-	 * 
+	 *
 	 * @throws RserveException
 	 * @throws InterruptedException
 	 * @throws IOException
